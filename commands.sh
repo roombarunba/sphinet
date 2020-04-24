7fa9309e33f0e82d6eeb1998b57d6c35b5759c71
roombarunba <hitokagekawaiize@gmail.com>
2020-04-24 10:59:24 +0900

    シェルにして複数コマンド扱えるかのテストだよ

---
+++ commands.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Test
+echo "TestDayooooooooooooooooooooo!!!!!!!!!!!!!!!!!!!!!!"
+
+# Test
+echo "sphinx-build source build"
+
+# sphinx-build source build
+sphinx-build source build
